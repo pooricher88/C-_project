5279cb3cafd0fdb4fb52adfa21619c05d3046cd3
DESKTOP-AEDU3K3\сучара <mazurdima07@gmail.com>
2025-11-29 18:20:25 +0200

    no message

---
+++ Lab4/SmartHomeSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SmartHomeSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            SmartHomeController controller = new SmartHomeController();
+
+            Light lamp = new Light { Name = "Лампа у вітальні" };
+            AirConditioner ac = new AirConditioner { Name = "Кондиціонер у спальні" };
+            CoffeeMachine coffee = new CoffeeMachine { Name = "Кавомашина на кухні" };
+            MotionSensor sensor = new MotionSensor { Name = "Датчик руху у коридорі" };
+
+            controller.AddDevice(lamp);
+            controller.AddDevice(ac);
+            controller.AddDevice(coffee);
+            controller.AddDevice(sensor);
+
+            controller.AddEnergyDevice(lamp);
+            controller.AddEnergyDevice(ac);
+            controller.AddEnergyDevice(coffee);
+
+            controller.TurnAllOn();
+
+            lamp.PrintStatus();
+            ac.PrintStatus();
+            coffee.PrintStatus();
+            sensor.PrintStatus();
+
+            controller.ShowEnergyReport(5);
+
+            controller.TurnAllOff();
+        }
+    }
+}
---
+++ Lab4/SmartHomeSystem/SmartHomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeSystem
+{
+    public class SmartHomeController
+    {
+        private readonly List<ISwitchable> allDevices = new List<ISwitchable>();
+        private readonly List<IEnergyConsumer> energyDevices = new List<IEnergyConsumer>();
+
+        public void AddDevice(ISwitchable device)
+        {
+            allDevices.Add(device);
+        }
+
+        public void AddEnergyDevice(IEnergyConsumer device)
+        {
+            energyDevices.Add(device);
+        }
+
+        public void TurnAllOn()
+        {
+            foreach (var d in allDevices)
+                d.TurnOn();
+        }
+
+        public void TurnAllOff()
+        {
+            foreach (var d in allDevices)
+                d.TurnOff();
+        }
+
+        public void ShowEnergyReport(int hours)
+        {
+            Console.WriteLine($"\nЗвіт про споживання енергії за {hours} год:");
+
+            double total = 0;
+
+            foreach (var d in energyDevices)
+            {
+                double used = d.GetEnergyUsage(hours);
+                total += used;
+
+                Console.WriteLine($"{d.DeviceName}: {used:F2} кВт·год (потужність: {d.PowerConsumption} Вт)");
+            }
+
+            Console.WriteLine($"\nЗагальне споживання: {total:F2} кВт·год");
+            Console.WriteLine($"Вартість (~4 грн/кВт·год): {total * 4:F2} грн\n");
+        }
+    }
+}
